19d6cdd10f419798eaa19d8d560c88b40245150a
A Furry Account <ender@thefluegges.com>
2020-12-01 14:32:14 -0500

    init

---
+++ src/main/java/com/hardcorecraft/plugins/lobbymanager/Lobbymanager.java
@@ -0,0 +1,17 @@
+package com.hardcorecraft.plugins.lobbymanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Lobbymanager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
